fee11a5eb83c320ac24de15fb63b11b357f6cdb2
Victoria Versteegh <remite2243@gmail.com>
2023-11-27 15:21:16 -0500

    added cards and basic setup

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var path = require('path');
+
+var applicationRouter = require('./routes/application');
+var publicRouter = require('./routes/public');
+
+var app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/application/', applicationRouter);
+app.use('/', publicRouter);
+
+
+const PORT  = process.env.PORT || 3050
+app.listen(PORT,()=> console.info(`Server has started on ${PORT}`));
+
+
+module.exports = app;

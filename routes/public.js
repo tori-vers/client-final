80ec4272e2b603ed82136644938d4364d5d1fe3f
Ves McVoid <35824499+Arithkauv@users.noreply.github.com>
2023-11-28 18:31:00 -0500

    Update public.js

--- routes/public.js
+++ routes/public.js
@@ -7,12 +7,16 @@ router.get('/', function(req, res, next) {
     console.log("Public: / started!");
 
     res.sendFile(path.resolve('public/index.html') );
+
+    console.log("Public: / done!");
 });
 
 router.get('/*', function(req, res, next) {
     console.log("Public: /* started!");
 
     res.sendFile(path.resolve('public/' + req.url) );
+
+    console.log("Public: /* done!");
 });
 
 

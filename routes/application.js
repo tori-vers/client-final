35337680097b8a90b8bac17a1e8a07101fe5a1a9
Ves McVoid <35824499+Arithkauv@users.noreply.github.com>
2023-11-28 18:29:50 -0500

    Update application.js

--- routes/application.js
+++ routes/application.js
@@ -11,6 +11,8 @@ router.get('/character', function(req, res, next) {
     appController = new applicationController();
     html = appController.getAllCharacters();
     res.send(html);
+
+    console.log("(Routes) Application: /character done!");
 });
 
 router.get('/character/:id', function(req, res, next) {
@@ -19,6 +21,8 @@ router.get('/character/:id', function(req, res, next) {
     appController = new applicationController();
     html = appController.getCharacterById(req.params.id);
     res.send(html);
+
+    console.log("(Routes) Application: /character/:id done!");
 });
 
 

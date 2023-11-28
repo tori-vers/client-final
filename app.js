9c90882c7ad2c3b4d8faf4d8df616e35b58f8cf4
Ves McVoid <35824499+Arithkauv@users.noreply.github.com>
2023-11-28 18:23:17 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3,16 +3,20 @@ var path = require('path');
 
 var applicationRouter = require('./routes/application');
 var publicRouter = require('./routes/public');
+console.log("App: requirements done.");
 
 var app = express();
+console.log("App: express() done.");
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/application/', applicationRouter);
 app.use('/', publicRouter);
+console.log("App: app.use() done.");
 
 
 const PORT  = process.env.PORT || 3054
 app.listen(PORT,()=> console.info(`Server has started on ${PORT}`));
+console.log("App: app.listen() done.");
 
 
 module.exports = app;

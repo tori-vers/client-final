e466986ac4d76bda9c9dc7ab85866026e149f85b
Ves McVoid <35824499+Arithkauv@users.noreply.github.com>
2023-11-28 18:47:08 -0500

    more debugging statements

--- application/applicationController.js
+++ application/applicationController.js
@@ -2,38 +2,47 @@ const fs = require('fs');
 
 class applicationController {
 
-  constructor(req) {
+    constructor(req) {
+        console.log("ApplicationController: constructor() started");
 
-    try {
-    //  console.log("Reading JSON");
-    var rawData = fs.readFileSync("application/data/applicationData.json");
-     // console.log("parsing");
-    this.finalData = JSON.parse(rawData);
-    //  console.log("Loaded", this.finalData);
-  } catch(error) {
-    console.error("Error with reading/parsing JSON file", error);
-  }}
+        try {
+            console.log("Reading JSON");
+            var rawData = fs.readFileSync("application/data/applicationData.json");
+
+            console.log("parsing");
+            this.finalData = JSON.parse(rawData);
+
+            console.log("Loaded", this.finalData);
+        } catch(error) {
+            console.error("Error with reading/parsing JSON file", error);
+        }
+    }
 
     getAllCharacters() {
+      console.log("ApplicationController: getAllCharacters() started");
+
       //return all characters data
       return this.finalData;
     }
 
     getCharacterById(id) {
+        console.log("ApplicationController: getCharacterById() started");
 
-  // if the id is valid (between 0 and the end of the values)
-  if (id >= 0 && id < this.finalData.length) {
-    const character = this.finalData[id];
-    return character; 
-  } else {
-    return { error: "finalData character not found" , id};
-  }
+        // if the id is valid (between 0 and the end of the values)
+        if (id >= 0 && id < this.finalData.length) {
+            const character = this.finalData[id];
+            return character; 
+        } else {
+            return { error: "finalData character not found" , id};
+        }
+    }
+
+    getCharacterByUni(uni) {
+        console.log("ApplicationController: getCharacterByUni() started");
+        const universe = this.finalData[uni];
+        return universe;
+    }
+
 }
 
-  getCharacterByUni(uni) {
-    const universe = this.finalData[uni];
-    return universe;
-  }
-
-}
 module.exports = applicationController;
